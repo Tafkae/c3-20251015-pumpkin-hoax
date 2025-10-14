f90e7f7c83943ee2c03516ad302da476c03e9574
Jim Kriz <jim@kriz.net>
2025-10-13 20:50:17 -0400

    TypeScript solution template

--- Templates/TypeScript/src/kata.ts
+++ Templates/TypeScript/src/kata.ts
@@ -1,5 +1,18 @@
+// This is just a simple type to hold x and y coordinates
+export type Point = [number, number];
+
 export class Kata {
     public hello(name: string): string {
         return `Hello ${name}`;
+    }
+
+    public loadMap(filename: string): Array<Array<string>> {
+        const fs = require('fs');
+        const data = fs.readFileSync(filename, 'utf8');
+        return data.split('\n').map((line: string) => line.split(''));
+    }
+
+    public findShortestPath(cornMaze: Array<Array<string>>): Array<Point> {
+        return []; // No path found
     }
 }
