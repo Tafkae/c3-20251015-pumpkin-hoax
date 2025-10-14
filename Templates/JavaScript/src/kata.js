bb6c2f7768927d659d0ce286a6aa23a53be55b21
Jim Kriz <jim@kriz.net>
2025-10-13 20:44:11 -0400

    JavaScript solution template

--- Templates/JavaScript/src/kata.js
+++ Templates/JavaScript/src/kata.js
@@ -2,6 +2,16 @@ class Kata {
     hello(name) {
         return `Hello ${name}`;
     }
+
+    loadMap(filename) {
+        const fs = require('fs');
+        const data = fs.readFileSync(filename, 'utf8');
+        return data.split('\n').map((line) => line.split(''));
+    }
+
+    findShortestPath(cornMaze) {
+        return []; // No path found
+    }
 }
 
 module.exports = Kata;
